1fbf7729b3571875f0bddf88fbdea8c2e0c0a780
Diana <diana.ribac001@gmail.com>
2024-04-18 17:52:28 +0300

    add views and implement controllers

--- LearnMVC/MvcBasics/Controllers/ClubController.cs
+++ LearnMVC/MvcBasics/Controllers/ClubController.cs
@@ -1,13 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MvcBasics.Data;
 
 namespace MvcBasics.Controllers;
 
 public class ClubController : Controller
 {
+    private readonly AppDbContext _context;
+    public ClubController(AppDbContext context)
+    {
+        _context = context;
+    }
     public IActionResult Index()
     {
-        return View();
+        return View(_context.Clubs.ToList());
     }
-
-
 }
---
+++ LearnMVC/MvcBasics/Controllers/RaceController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcBasics.Data;
+
+namespace MvcBasics.Controllers
+{
+    public class RaceController : Controller
+    {
+        private readonly AppDbContext _context;
+        public RaceController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View(_context.Races.ToList());
+        }
+    }
+}
